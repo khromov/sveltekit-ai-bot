8436d8a5845c0a881acc75981479eb51bca5c410
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-07-16 23:13:03 +0200

    wip

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Toaster } from 'svelte-french-toast';
+</script>
+
+<Toaster />
+
+<slot />
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,17 @@
 import { ANTHROPIC_API_KEY } from '$env/static/private';
 import Anthropic from '@anthropic-ai/sdk';
 import type { Actions } from './$types';
+import type { TextBlock } from '@anthropic-ai/sdk/resources/messages.mjs';
 
 const anthropic = new Anthropic({
 	apiKey: ANTHROPIC_API_KEY,
 });
 
-/*
 export const actions: Actions = {
 	default: async ({ request }) => {
-        console.log("woop");
 		const data = await request.formData();
 		const userMessage = data.get('message') as string;
-		const chatHistory = JSON.parse(data.get('chat_history') as string || '[]') as Message[];
+		const chatHistory = JSON.parse(data.get('chat_history') as string || '[]');
 
 		chatHistory.push({ role: 'user', content: userMessage });
 
@@ -23,7 +22,9 @@ export const actions: Actions = {
 			messages: chatHistory,
 		});
 
-		const assistantMessage = { role: 'assistant', content: response.content[0]?.text || '' };
+        const blocks = response.content as TextBlock[];
+
+		const assistantMessage = { role: 'assistant', content: blocks[0].text || '' };
 		chatHistory.push(assistantMessage);
 
 		return {
@@ -31,4 +32,3 @@ export const actions: Actions = {
 		};
 	},
 };
-*/
